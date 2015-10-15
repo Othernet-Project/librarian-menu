3c972c90fd5a8b852b153d88e11e554ceca8496d
Branko Vukelic <branko@outernet.is>
2015-10-15 13:24:28 +0200

    Removed rendering logic, allow bitmap icons

--- librarian_menu/menu.py
+++ librarian_menu/menu.py
@@ -14,45 +14,72 @@ from bottle_utils.i18n import i18n_url
 
 
 class MenuItem(object):
-    label = ''
+    """ Represents a menu item """
+
+    #: Menu group
     group = None
-    icon_class = ''
-    alt_icon_class = ''
+
+    #: Menu item label (use lazy object to make it dynamic)
+    label = ''
+
+    #: Named route which menu item targets
     route = None
-    default_classes = ('navicon',)
+
+    #: Menu item identifier
     name = None
 
+    #: Whether icon is a bitmap (set to ``False`` to use icon classes instead)
+    icon_is_bitmap = False
+
+    #: Menu icon class
+    icon_class = ''
+
+    #: Alternative state menu item class (see ``is_alt_icon_visible()``)
+    alt_icon_class = ''
+
+    #: Path to the icon bitmap (path is under ``sys:static`` route)
+    icon_bitmap_path = None
+
+    #: Path to alternative bitmap (path is under ``sys:static``)
+    alt_icon_bitmap_path = None
+
     def is_alt_icon_visible(self):
+        """ Whether alternative icon is selected
+
+        Overload this method in subclass to customize the condition under which
+        the alternative icon is selected.
+        """
         return False
 
     def is_visible(self):
+        """ Whether icon is rendered in the UI
+
+        Overload this method in subclass to customize the condition under which
+        the visibility is enabled or disabled.
+        """
         return True
 
     def get_path(self):
+        """ Return the full target path based on the route
+
+        If you need to conditionally construct the path, or pass it additional
+        parameters, overload this method in your subclass.
+        """
         return i18n_url(self.route)
 
-    def render(self):
-        if not self.is_visible():
-            return ''
+    @class
+    def active_icon_class(self):
+        """ Active class for the icon based on ``is_alt_icon_visible`` """
         if self.is_alt_icon_visible():
-            icon_class = self.alt_icon_class
-        else:
-            icon_class = self.icon_class
-        if icon_class:
-            icon = html.SPAN(_class="icon")
-        else:
-            icon = ''
+            return self.alt_icon_class
+        return self.icon_class
 
-        item_class = ' '.join(
-            tuple(self.default_classes) + (icon_class,)).strip()
-        return html.link_other(
-            ' '.join([icon + html.SPAN(self.label, _class="label")]),
-            self.get_path(), request.original_path,
-            lambda s, _class: html.SPAN(s, _class='active ' + item_class),
-            _class=item_class)
+    @class
+    def active_icon_bitmap_path(self):
+        """ Active path for the icon based on ``is_alt_icon_visible`` """
+        if self.is_alt_icon_visible():
+            return self.alt_icon_bitmap_path
+        return self.icon_bitmap_path
 
     def __str__(self):
-        return self.render()
-
-    def __unicode__(self):
-        return self.render()
+        return '<MenuItem "{}" for {}>'.format(self.name, self.route)
